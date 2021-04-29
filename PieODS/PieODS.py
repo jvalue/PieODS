6f8395c20b777180114798b39fe4fe7100583660
Shady-Hegazy <shadi.hegazy@gmail.com>
2021-04-29 11:14:24 +0200

    Pospone refactoring

--- PieODS/PieODS.py
+++ PieODS/PieODS.py
@@ -1,13 +1,13 @@
-import Adapter
-import Pipeline
-import Notification
-import Storage
+# import Adapter
+# import Pipeline
+# import Notification
+# import Storage
 
-ad = Adapter.AdapterAPI()
-ds = Adapter.DatasourceAPI()
-pl = Pipeline.PipelineAPI()
-nt = Notification.NotificationAPI()
-st = Storage.StorageAPI()
+# ad = Adapter.AdapterAPI()
+# ds = Adapter.DatasourceAPI()
+# pl = Pipeline.PipelineAPI()
+# nt = Notification.NotificationAPI()
+# st = Storage.StorageAPI()
 
-class DataSource():
-    pass
+# class DataSource():
+#     pass
