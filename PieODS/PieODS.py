5a5bddc642be6757e1c1068fe0dfce178dd85fcb
Shady-Hegazy <shadi.hegazy@gmail.com>
2021-05-02 15:18:18 +0200

    Commenting the layer development for the moment

--- PieODS/PieODS.py
+++ PieODS/PieODS.py
@@ -2,16 +2,17 @@
 # import .Pipeline
 # import .Notification
 # import .Storage
-from . import Adapter
-from . import Pipeline
-from . import Notification
-from . import Storage
+# from . import Adapter
+# from . import Pipeline
+# from . import Notification
+# from . import Storage
 
-ad = Adapter.AdapterAPI()
-ds = Adapter.DatasourceAPI()
-pl = Pipeline.PipelineAPI()
-nt = Notification.NotificationAPI()
-st = Storage.StorageAPI()
+# ad = Adapter.AdapterAPI()
+# ds = Adapter.DatasourceAPI()
+# pl = Pipeline.PipelineAPI()
+# nt = Notification.NotificationAPI()
+# st = Storage.StorageAPI()
 
-class DataSource():
-    pass
+# # class DataSource():
+# #     def __init__(self, protocol_type:str="HTTP", location:str=None, encoding:str="UTF-8", default_parameters=) -> None:
+# #         pass
